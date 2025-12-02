a255eb3181ecd5388c044d91651c6066442e2793
dmartinezsanchez79 <dmartinezsanchez79@gmail.com>
2025-12-02 17:42:01 +0100

    Añadir proyecto Maven y CI

---
+++ src/test/java/com/ucam/CalculadoraTest.java
@@ -0,0 +1,20 @@
+package com.ucam;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CalculadoraTest {
+
+    @Test
+    void testSumar() {
+        Calculadora calc = new Calculadora();
+        assertEquals(5, calc.sumar(2, 3));
+    }
+
+    @Test
+    void testRestar() {
+        Calculadora calc = new Calculadora();
+        assertEquals(1, calc.restar(3, 2));
+    }
+}
