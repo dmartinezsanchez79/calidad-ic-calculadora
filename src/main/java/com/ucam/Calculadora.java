55055aa3d68a1144e354eedd3d2c76d70abc4d6e
dmartinezsanchez79 <dmartinezsanchez79@gmail.com>
2025-12-02 18:04:15 +0100

    Añadir javadoc y corregir estilo

--- src/main/java/com/ucam/Calculadora.java
+++ src/main/java/com/ucam/Calculadora.java
@@ -1,11 +1,28 @@
 package com.ucam;
 
+/**
+ * Calculadora sencilla para realizar operaciones aritméticas básicas.
+ */
 public class Calculadora {
 
+    /**
+     * Suma dos números enteros.
+     *
+     * @param a primer sumando
+     * @param b segundo sumando
+     * @return resultado de a + b
+     */
     public int sumar(int a, int b) {
         return a + b;
     }
 
+    /**
+     * Resta dos números enteros.
+     *
+     * @param a minuendo
+     * @param b sustraendo
+     * @return resultado de a - b
+     */
     public int restar(int a, int b) {
         return a - b;
     }
